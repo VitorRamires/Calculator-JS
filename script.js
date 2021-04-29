cdd2d2f99ae56210e2f6763b4c99b4f52ec4d7ad
Vitor <vitorm.ramires@hotmail.com>
2021-04-29 11:21:42 -0300

    adicionando botao limpar

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 
+var input1 = document.getElementById("n1")
+var input2 = document.getElementById("n2")
+var inputSoma = document.getElementById("Soma")
+var inputSubtração = document.getElementById("Sub")
+var inputSubtraçãoInvert = document.getElementById("SubInvertida")
+var multiplicação = document.getElementById("Multi")
+var divisão = document.getElementById("Divi")
+var divisãoInvert = document.getElementById("DiviInvertida")
+var potencia = document.getElementById("Pot1")
+var potencia2 = document.getElementById("Pot2")
+var divisor1 = document.getElementById("Divisor1")
+var divisor2 = document.getElementById("Divisor2")
+var fatorial1 = document.getElementById("Fatorial1")
+var fatorial2 = document.getElementById("Fatorial2")
 
 function limpar(){
-    
+    input1.value = "";
+    input2.value = "";
+    inputSoma.value = "";
+    inputSubtração.value = "";
+    inputSubtraçãoInvert.value = "";
+    multiplicação.value = "";
+    divisão.value = "";
+    divisãoInvert.value = "";
+    potencia2.value = "";
+    potencia.value = "";
+    divisor1.value = "";
+    divisor2.value = "";
+    fatorial1.value = "";
+    fatorial2.value = "";
 }
 
 function enviar(){
 
-var input1 = document.getElementById("n1")
-var input2 = document.getElementById("n2")
 if(input1.value.length == 0 || input2.value.length == 0){
     alert("Adicione os numeros A e B!")
 } 
@@ -18,32 +43,32 @@ var getInput2 = parseFloat(input2.value)
 
 
 //soma
-var inputSoma = document.getElementById("Soma")
+
 somando = getInput2 + getInput1
 inputSoma.value = `${somando}`
 console.log(somando)
 
 
 //subtração
-var inputSubtração = document.getElementById("Sub")
+
 subtraindo = getInput1 - getInput2
 inputSubtração.value = `${subtraindo}`
 
 //Subtração Invertida
-var inputSubtraçãoInvert = document.getElementById("SubInvertida")
+
 subtraçãoInvertida = getInput2 - getInput1
 inputSubtraçãoInvert.value = `${subtraçãoInvertida}`
 
 
 //multiplicação
-var multiplicação = document.getElementById("Multi")
+
 multiplicando = getInput1 * getInput2
 multiplicação.value = `${multiplicando}`
 
 
 
 //divisão
-var divisão = document.getElementById("Divi")
+
 if(getInput2 === 0){
     divisão.value = "Divisão por 0!"
 } else {
@@ -52,7 +77,7 @@ divisão.value = `${dividindo}`
 }
 
 //divisão invertida
-var divisãoInvert = document.getElementById("DiviInvertida")
+
 if(getInput1 === 0){
     divisãoInvert.value = "Divisão por 0!"
 } else {
@@ -62,18 +87,18 @@ divisãoInvert.value = `${dividindoInvertido}`
 
 
 //potencia N1
-var potencia = document.getElementById("Pot1")
+
 potenciando = getInput1**2
 potencia.value = `${potenciando}`
 
 //Potencia N2
-var potencia2 = document.getElementById("Pot2")
+
 potenciando2 = getInput2**2
 potencia2.value = `${potenciando2}`
 
 
 //Divisores inteiros N1
-var divisor1 = document.getElementById("Divisor1")
+
 var valores = []
 divisor1.value = ""
 if(getInput1 >= 0){
@@ -91,7 +116,7 @@ divisor1.value += `Total(${valores.length})`
 
 
 //Divisores inteiros N2
-var divisor2 = document.getElementById("Divisor2")
+
 var valores2 = []
 divisor2.value = ""
 if(getInput2 >= 0){
@@ -109,7 +134,7 @@ divisor2.value += `Total(${valores2.length})`
 
 
 //fatorial N1
-var fatorial1 = document.getElementById("Fatorial1")
+
 fatIni = 1
 fatorial1.value = ""
 if(getInput1 <= 21 && getInput1 >= 0){
@@ -124,7 +149,7 @@ fatorial1.value = `${fatIni}`
 
 
 //fatorial N2
-var fatorial2 = document.getElementById("Fatorial2")
+
 fatIni2 = 1
 fatorial2.value = ""
 if(getInput2 <= 21 && getInput2 >= 0){
